42efd3747f825dace2e9022586bd3b3cb66ebde8
Minminjamin <0429el@gmail.com>
2023-03-09 15:10:42 +0900

    3nd upload

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-export const store = configureStore({
-  reducer: {},
-})
-// The store now has redux-thunk added and the Redux DevTools Extension is turned on
